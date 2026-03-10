41e6eee10cee3aa3b2c6afe4367075427aeed874
okanzen1 <okanozen1@hotmail.com>
2026-03-10 20:07:01 +0300

    Google transalte eklendi

--- app/Services/TranslatorService.php
+++ app/Services/TranslatorService.php
@@ -1,25 +1,46 @@
 <?php
 
 namespace App\Services;
-
+use Stichoza\GoogleTranslate\GoogleTranslate;
 use App\Models\Translator;
 use Illuminate\Support\Facades\Http;
 
 class TranslatorService
 {
-    public function translateSingle($text,$lang)
-    {
-        $response = Http::withHeaders([
-            'Authorization' => 'DeepL-Auth-Key '.config('services.deepl.key')
-        ])->asForm()->post(
-            'https://api-free.deepl.com/v2/translate',
-            [
-                'text'=>$text,
-                'source_lang'=>'EN',
-                'target_lang'=>strtoupper($lang)
-            ]
-        );
+    // public function translateSingle($text,$lang)
+    // {
+    //     $response = Http::withHeaders([
+    //         'Authorization' => 'DeepL-Auth-Key '.config('services.deepl.key')
+    //     ])->asForm()->post(
+    //         'https://api-free.deepl.com/v2/translate',
+    //         [
+    //             'text'=>$text,
+    //             'source_lang'=>'EN',
+    //             'target_lang'=>strtoupper($lang)
+    //         ]
+    //     );
 
-        return $response->json('translations.0.text') ?? $text;
+    //     return $response->json('translations.0.text') ?? $text;
+    // }
+
+    public function translateSingle($text, $lang)
+    {
+        if (!$text) {
+            return '';
+        }
+
+        try {
+
+            $tr = new GoogleTranslate();
+            $tr->setSource('en');
+            $tr->setTarget($lang);
+
+            return $tr->translate($text);
+        } catch (\Exception $e) {
+
+            \Log::error('Translation error: ' . $e->getMessage());
+
+            return $text;
+        }
     }
 }
